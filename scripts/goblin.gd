6bfc582a9a1321077eb1285e65a86c1a833dcd98
OnlyEntity <onelyianalone@gmail.com>
2025-11-08 17:23:08 +0800

    Adding enemy basic enemy movement

---
+++ scripts/goblin.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+const BASE_SPEED = 300
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+var player_post
+
+func _ready():
+	player_post = get_node("/root/mainGame/Player")
+
+func _physics_process(delta):
+	var direction = global_position.direction_to(player_post.global_position)
+	velocity = direction * BASE_SPEED
+	move_and_slide()
+	if velocity.x < 0:
+		animated_sprite_2d.flip_h = true
+	else :
+		animated_sprite_2d.flip_h = false
+	
