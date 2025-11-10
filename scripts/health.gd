3273de67e6b7c9115a55c47c7c1ff500dc3fc4ea
Veuron <developerrezki@gmail.com>
2025-11-10 17:41:51 +0800

    add item + fight sistem + some effect

--- scripts/health.gd
+++ scripts/health.gd
@@ -1,15 +1,15 @@
 extends Area2D
 
-@onready var animate = $apple_image/animation
+@onready var animate = $health_image/animation
 
 func _ready() -> void:
-	animate.play("apple_spawn")
+	animate.play("health_spawn")
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		GlobalVar.apple += 1
-		animate.play("apple_taken")
+		GlobalVar.healthPlayer += 30
+		animate.play("health_taken")
 		
 func _on_animation_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "apple_taken":
+	if anim_name == "health_taken":
 		queue_free()
