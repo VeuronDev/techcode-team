9ed8f20ec87804c002291d7b055c49150f7754d7
Veuron <developerrezki@gmail.com>
2025-11-21 15:06:01 +0800

    dialog

---
+++ scripts/call_dialog.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+
+@onready var name_tag = $border_dialog/tag_name
+@onready var char_img = $border_dialog/char
+@onready var text_dialog = $text
+@onready var next = $next
+@onready var back = $back
+
+func _ready() -> void:
+	pass
+
+func text_dialog_main(text) -> void:
+	pass
+
+func char_img_main(path_img) -> void:
+	pass
+
+func name_tag_main(name_tag) -> void:
+	pass
+	
+func call_dialogs(text, path_img, name_tag, number) -> void:
+	pass
+	
---
+++ scripts/dialog.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var dialog_chapter_1 = {
+	"1": {
+		"text": "text",
+		"name_tag": "name",
+		"path_img": "path.png"
+	},
+	"2": {
+		"text": "text",
+		"name_tag": "name",
+		"path_img": "kepala_desa.png"
+	}
+}
